eeccfc05c6d95ab37ec658d8a6d3ac771daf59ab
davidrogger <davidrogger.a@gmail.com>
2023-03-16 14:32:34 -0300

    FEAT: array render elements

--- 01-initial-use-installing/CLI/src/components/Info.vue
+++ 01-initial-use-installing/CLI/src/components/Info.vue
@@ -2,15 +2,27 @@
   <div>
     <p>Linguagens:</p>
     <ul>
-      <li>Javascript</li>
-      <li>ReactJS</li>
-      <li>Python</li>
+      <li
+        v-for="{ name, id } in technologies"
+        :key="id"
+      >
+      {{ name }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'InfoComponent'
+    name: 'InfoComponent',
+    data() {
+      return {
+        technologies: [
+          { id: 1, name: 'Javascript' },
+          { id: 2, name: 'ReactJS' },
+          { id: 3, name: 'Python' },
+        ]
+      }
+    }
   }
 </script>
