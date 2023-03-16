8e6bcc1b024271f2ee52a9cbcc7b176db3c7021a
davidrogger <davidrogger.a@gmail.com>
2023-03-16 14:04:06 -0300

    FEAT: hidden email button with phosphor

--- 01-initial-use-installing/CLI/src/components/AttributesDina.vue
+++ 01-initial-use-installing/CLI/src/components/AttributesDina.vue
@@ -3,7 +3,27 @@
     class="session"
   >
   <h1>Atributos Dinâmicos</h1>
-  <p v-show="show_email"> Meu e-mail é: {{ email }} </p>
+  <div
+    class="email-container"
+  >
+    Meu e-mail é:
+  <div
+    class="email-display"
+  >
+  <span
+  v-show="show_email"
+  >
+    {{ email }}
+  </span>
+  </div>
+  <div
+    class="eye-icon"
+    @click="toggleEmail"
+  >
+    <span v-if="show_email"><PhEyeClosed /></span>
+    <span v-else><PhEye /></span>
+  </div>
+  </div>
   <p>Acesse meu link <a v-bind:href="link" target="_blank">aqui</a></p>
   <PictureComponent />
   </div>
@@ -11,6 +31,7 @@
 
 <script>
 import PictureComponent from './Picture.vue';
+import { PhEye, PhEyeClosed } from '@phosphor-icons/vue';
 
   export default {
     nome: 'AttributesDina',
@@ -18,11 +39,18 @@ import PictureComponent from './Picture.vue';
       return {
         show_email: true,
         email: 'jonas@doe.com',
-        link: 'https://www.google.com'
+        link: 'https://www.google.com',
       }
     },
     components: {
-      PictureComponent
+      PictureComponent,
+      PhEye,
+      PhEyeClosed
+    },
+    methods: {
+      toggleEmail() {
+        this.show_email = !this.show_email;
+      }
     }
   }
 </script>
