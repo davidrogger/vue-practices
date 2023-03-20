1e07578e5a89b69f2fe0f930ea28c8317062014a
davidrogger <davidrogger.a@gmail.com>
2023-03-20 15:42:55 -0300

    FEAT: footer component

--- 01-initial-use-installing/make-your-burger/src/App.vue
+++ 01-initial-use-installing/make-your-burger/src/App.vue
@@ -1,15 +1,22 @@
-<script setup>
+<script>
   import { RouterLink, RouterView } from 'vue-router'
+  import Footer from './components/Footer.vue';
   import Navbar from './components/Navbar.vue';
+
+  export default {
+    components: {
+      Navbar,
+      Footer,
+    }
+  }
 </script>
 
 <template>
-    <div class="wrapper">
-      <Navbar />
-      <RouterView />
-    </div>
+  <div>
+    <Navbar />
+    <RouterView />
+    <Footer />
+  </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
---
+++ 01-initial-use-installing/make-your-burger/src/components/Footer.vue
@@ -0,0 +1,11 @@
+<script>
+  export default {
+    name: 'Footer'
+  }
+</script>
+
+<template>
+  <footer>
+    Footer
+  </footer>
+</template>
