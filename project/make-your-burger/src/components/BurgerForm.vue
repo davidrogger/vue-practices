d1cdfec068d896a147c45e01a4a912ddff30c2ab
davidrogger <davidrogger.a@gmail.com>
2023-03-21 09:38:54 -0300

    FEAT: created component BurgerForm

---
+++ project/make-your-burger/src/components/BurgerForm.vue
@@ -0,0 +1,97 @@
+<script>
+  export default {
+    name: "BurgerForm"
+  }
+</script>
+
+<template>
+  <div>
+    <span>
+      Action message
+    </span>
+      <form>
+        <div class="mandaroty-form-container">
+          <label for="customerName">
+            Customer Name
+            <input
+              type="text"
+              name="customerName"
+              id="customerName"
+              v-model="customerName"
+              placeholder="Insert your name here"
+            >
+          </label>
+          <label for="breadPick">
+            Pick your bread
+            <select name="breadPick" id="breadPick">
+              <option value="" disabled selected>
+                Pick a bread here
+              </option>
+            </select>
+          </label>
+          <label for="meatPick">
+            Pick your meat
+            <select name="meatPick" id="meatPick">
+              <option value="" disabled selected>
+                Pick a meat here
+              </option>
+            </select>
+          </label>
+        </div>
+        <div class="extra-form-container">
+          <h2>Extra ingredients</h2>
+          <div class="extra-options-container">
+            <label for="first">
+              <input id="first" type="checkbox">
+              first item
+            </label>
+            <label for="second">
+              <input id="second" type="checkbox">
+              second item
+            </label>
+            <label for="third">
+              <input id="third" type="checkbox">
+              Third item
+            </label>
+          </div>
+        </div>
+      </form>
+  </div>
+</template>
+
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+    max-width: 350px;
+    margin: auto;
+  }
+
+  .mandaroty-form-container label,
+  .extra-form-container {
+    display: flex;
+    flex-direction: column;
+    border-left: 4px solid var(--primary-accent-color);
+    margin-bottom: 15px;
+    padding: 5px 10px;
+    color: var(--primary-dark-color);
+    font-weight: bold;
+  }
+
+  .mandaroty-form-container input,
+  .mandaroty-form-container select {
+    padding: 10px;
+    margin-top: 5px;
+  }
+
+  .extra-form-container h2 {
+    margin-bottom: 10px;
+  }
+
+  .extra-options-container {
+    display: flex;
+    flex-flow: row wrap;
+    gap: 10px;
+    width: 100%;
+  }
+</style>
--- project/make-your-burger/src/views/HomeView.vue
+++ project/make-your-burger/src/views/HomeView.vue
@@ -1,18 +1,21 @@
 
 <script>
   import Banner from '../components/Banner.vue';
+  import BurgerForm from '../components/BurgerForm.vue';
 
   export default {
     name: "HomeView",
     components: {
-      Banner,
-    }
+    Banner,
+    BurgerForm
+}
   }
 </script>
 
 <template>
   <Banner />
   <main class="page-container">
-    <h1>Home Page!</h1>
+    <h1>Build your own burger!</h1>
+    <BurgerForm />
   </main>
 </template>
