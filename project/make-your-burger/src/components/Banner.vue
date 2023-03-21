b7716a0a69bd816097f0cc8144e7b78f1300ee29
davidrogger <davidrogger.a@gmail.com>
2023-03-21 08:42:18 -0300

    FEAT: created banner component

---
+++ project/make-your-burger/src/components/Banner.vue
@@ -0,0 +1,32 @@
+<script>
+  export default {
+    name: "Banner"
+  }
+</script>
+
+<template>
+  <div class="banner-container">
+    <h1>Make Your Burger</h1>
+  </div>
+</template>
+
+<style scoped>
+  .banner-container {
+    background-image: url('/img/burger.jpg');
+    background-position: 0 65%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  h1 {
+    background-color: var(--primary-dark-color);
+    color: var(--primary-accent-color);
+    text-align: center;
+    font-size: 3.5rem;
+    padding: 20px 40px;
+  }
+</style>
--- project/make-your-burger/src/views/HomeView.vue
+++ project/make-your-burger/src/views/HomeView.vue
@@ -1,4 +1,17 @@
+
+<script>
+  import Banner from '../components/Banner.vue';
+
+  export default {
+    name: "HomeView",
+    components: {
+      Banner,
+    }
+  }
+</script>
+
 <template>
+  <Banner />
   <main class="page-container">
     <h1>Home Page!</h1>
   </main>
