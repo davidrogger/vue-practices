08b58102709920c4711aa69341cdead4ba067504
davidrogger <davidrogger.a@gmail.com>
2023-03-22 14:17:39 -0300

    FEAT: Loading component

---
+++ project/make-your-burger/src/components/Loading.vue
@@ -0,0 +1,53 @@
+<script>
+  export default {
+    name: "Loading"
+  }
+</script>
+
+<template>
+  <div class="lds-ring">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<style scoped>
+  .lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  }
+  .lds-ring div {
+    box-sizing: border-box;
+    top: 9px;
+    left: 32px;
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    border: 3px solid var(--primary-accent-color);
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: var(--primary-accent-color) transparent transparent transparent;
+  }
+  .lds-ring div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  .lds-ring div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  .lds-ring div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+</style>
